32ee6f421dd4fa91c85737a01ad813dcb2185928
gitdung <91383098+gitdung@users.noreply.github.com>
2022-12-19 17:53:41 +0700

    'hi'

---
+++ contact.php
@@ -0,0 +1,47 @@
+<?php 
+	include "head.php"
+	?>
+<?php
+$title ="Shop huy";
+$name ="Điện thoai";
+?>
+<?php 
+	include "top.php"
+    ?>
+    <?php 
+	include "Header.php"
+	?>
+	<?php 
+	include "navigation.php"
+	?>
+	<!--//////////////////////////////////////////////////-->
+	<!--///////////////////Contact Page///////////////////-->
+	<!--//////////////////////////////////////////////////-->
+	<div id="page-content" class="single-page">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<ul class="breadcrumb">
+						<li><a href="index.php">Home</a></li>
+						<li><a href="contact.php">Contact</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="heading"><h1>Liên hệ với chúng tôi</h1></div>
+				</div>
+				<div class="col-md-6">
+					<p><span class="glyphicon glyphicon-home"></span>Đà Nẵng, Việt Nam</p>
+					<p><span class="glyphicon glyphicon-earphone"></span> 012345678</p>
+					<p><span class="glyphicon glyphicon-envelope"></span> 123@vku.udn.vn</p>
+					<div class="mapouter"><div class="gmap_canvas"><iframe class="gmap_iframe" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=VKU&amp;t=&amp;z=17&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe><a href="https://piratebay-proxys.com/">Piratebay</a></div><style>.mapouter{position:relative;text-align:right;width:600px;height:400px;}.gmap_canvas {overflow:hidden;background:none!important;width:600px;height:400px;}.gmap_iframe {width:600px!important;height:400px!important;}</style></div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<?php 
+	include "footer.php"
+	?>
+</body>
+</html>
